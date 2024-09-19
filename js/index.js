44e6d52b4ff8f2607633963fb0a30068fb763c88
Raihan <aboraihan14@gmail.com>
2024-09-19 11:42:52 -0700

    some basic structure and validation added

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// const btn = document.getElementById('btn')
+// const inpNumber = document.getElementById('inpNumber')
+// const inpPin = document.getElementById('inpPin')
+
+document.getElementById('btn').addEventListener('click',function(event){
+    event.preventDefault()
+
+
+    const inpNumber = document.getElementById('inpNumber').value
+    console.log("Number",inpNumber)
+
+    const inpPin = document.getElementById('inpPin').value
+    console.log("Pin",inpPin)
+
+
+    if(inpNumber === "12" && inpPin === '12'){
+        console.log("Valid Input ")
+        window.location.href = '/home.html'
+    }
+    else{
+        alert("Invalid Input")
+    }
+
+   
+})
